199f862d6cdf293c47899650a7fbaf0c548879f9
Vicram <vicramanjohfue@gmail.com>
2024-10-31 14:55:46 +0200

    connect to db

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,15 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+// Load environment variables from .env file
+dotenv.config();
+
+// Connect to MongoDB
+mongoose
+  .connect(process.env.MONGODB_CONNECTION)
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.error("MongoDB connection error:", err));
 
 const app = express();
 

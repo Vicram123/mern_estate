034f93bb43905d203060dbd718fd51bf2389badc
Vicram <vicramanjohfue@gmail.com>
2024-11-01 00:09:03 +0200

    complet sign in page functionality

--- frontend/src/pages/SignIn.jsx
+++ frontend/src/pages/SignIn.jsx
@@ -1,3 +1,100 @@
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+
 export default function SignIn() {
-  return <div>SignIn</div>;
+  const [formData, setFormData] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [success, setSuccess] = useState(false);
+
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setError(null);
+    setSuccess(false);
+
+    // Simple client-side validation
+    if (!formData.email || !formData.password) {
+      setError("All fields are required.");
+      return;
+    }
+
+    try {
+      setLoading(true);
+      const res = await fetch("/api/auth/signin", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+
+      const data = await res.json();
+      setLoading(false);
+
+      if (data.success === false) {
+        setError(data.message);
+        return;
+      }
+
+      setSuccess(true);
+      setTimeout(() => {
+        navigate("/");
+      }, 2000); // Redirect after 2 seconds
+    } catch (error) {
+      setLoading(false);
+      setError(error.message);
+    }
+  };
+
+  return (
+    <>
+      <div className="p-3 max-w-lg mx-auto">
+        <div className="text-3xl text-center font-semibold my-7">Sign In</div>
+        <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+          <input
+            id="email"
+            type="email"
+            placeholder="email"
+            className="border p-3 rounded-lg"
+            onChange={handleChange}
+          />
+          <input
+            id="password"
+            type="password"
+            placeholder="password"
+            className="border p-3 rounded-lg"
+            onChange={handleChange}
+          />
+          <button
+            disabled={loading}
+            className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80"
+          >
+            {loading ? "Loading..." : "Sign In"}
+          </button>
+        </form>
+        <div className="flex gap-2 mt-5 ">
+          <p>Dont an account?</p>
+          <Link to={"/sign-up"}>
+            <span className="text-blue-700">Sign Up</span>
+          </Link>
+        </div>
+        {error && <p className="text-red-500 mt-5">{error}</p>}
+        {success && (
+          <p className="text-green-500 mt-5">
+            User created successfully! Redirecting...
+          </p>
+        )}
+      </div>
+    </>
+  );
 }

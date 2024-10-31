d0019f500069f769f2935217e3112c1ce8c78629
Vicram <vicramanjohfue@gmail.com>
2024-10-31 15:05:59 +0200

    create user model js

---
+++ backend/models/user.model.js
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [/.+@.+\..+/, "Please fill a valid email address"], // Simple email validation
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 6, // Minimum password length
+    },
+  },
+  { timestamps: true }
+);
+
+// Pre-save hook to hash the password
+userSchema.pre("save", async function (next) {
+  if (this.isModified("password")) {
+    this.password = await bcrypt.hash(this.password, 10);
+  }
+  next();
+});
+
+const User = mongoose.model("User", userSchema);
+
+export default User;

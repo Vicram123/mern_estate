2164bb506f3b4c0dbc172d131f07ccc7ab7e7bff
Vicram <vicramanjohfue@gmail.com>
2024-11-04 00:19:53 +0200

    creating update api route

--- backend/controllers/listing.controller.js
+++ backend/controllers/listing.controller.js
@@ -58,3 +58,24 @@ export const deleteListing = async (req, res, next) => {
     next(error); // Pass any errors to the error handling middleware
   }
 };
+
+export const updateListing = async (req, res, next) => {
+  const listing = await Listing.findById(req.params.id);
+  if (!listing) {
+    return next(errorHandler(404, "Listing not found"));
+  }
+  if (req.user.id !== listing.userRef.toString()) {
+    return next(errorHandler(401, "You can only update your own listings!"));
+  }
+  try {
+    const updatedListing = await Listing.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
+    // Send success response
+    return res.status(200).json(updatedListing);
+  } catch (error) {
+    next(error); // Pass any errors to the error handling middleware
+  }
+};

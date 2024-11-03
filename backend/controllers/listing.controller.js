6de81331235518123f86153970bd3c011dc9a32a
Vicram <vicramanjohfue@gmail.com>
2024-11-04 01:04:45 +0200

    complete update listing functionality

--- backend/controllers/listing.controller.js
+++ backend/controllers/listing.controller.js
@@ -79,3 +79,15 @@ export const updateListing = async (req, res, next) => {
     next(error); // Pass any errors to the error handling middleware
   }
 };
+
+export const getListing = async (req, res, next) => {
+  try {
+    const listing = await Listing.findById(req.params.id);
+    if (!listing) {
+      return next(errorHandler(404, "Listing not found"));
+    }
+    res.status(200).json(listing);
+  } catch (error) {
+    next(error); // Pass any errors to the error handling middleware
+  }
+};
